047c2fb8cb4d2dbcfff9b5f5c76ca5e110e10ed7
Irene <irroudnytska@gmail.com>
2022-01-04 01:09:59 +0200

    Create HWMainPageSelenideElements.java

---
+++ main/HWMainPageSelenideElements.java
@@ -0,0 +1,8 @@
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.$;
+
+public class HWMainPageSelenideElements {
+
+    SelenideElement laptopAndCompCategory = $(By.xpath("//li[@class='menu-categories__item ng-star-inserted']/a[@href=\"https://rozetka.com.ua/computers-notebooks/c80253/\"]"));
+}
