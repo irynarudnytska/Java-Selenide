226819b9d8f9662e5106157d01331056335f3444
Irene <irroudnytska@gmail.com>
2022-01-04 01:09:53 +0200

    Create HWCategoryPageSelenideElements.java

---
+++ main/HWCategoryPageSelenideElements.java
@@ -0,0 +1,7 @@
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import static com.codeborne.selenide.Selenide.$;
+
+public class HWCategoryPageSelenideElements {
+    SelenideElement laptopCategory = $(By.xpath("//a[@title='Ноутбуки']"));
+}
